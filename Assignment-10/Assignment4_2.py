e76c005fb2471381b1a5eaaa12655855aec6affb
Devashish Mayur Potnis <devashishpotnis.work@gmail.com>
2025-05-27 00:51:31 +0530

    Add files via upload

---
+++ Assignment-10/Assignment4_2.py
@@ -0,0 +1,14 @@
+
+multiplication =lambda a,b: a*b
+def main():
+    print("Enter First Nuber")
+    no1=int(input())
+
+    print("Enter Second Number")
+    no2=int(input())
+
+    answer=multiplication(no1,no2)
+    print("The Multiplication is :",answer)
+
+if __name__ =="__main__":
+    main()
